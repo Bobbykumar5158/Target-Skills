3c808bd4db91b658a1d79313c9d2dc87caf9c55a
BOBBY KUMAR <lpsbobbykumar5158@gmail.com>
2026-07-11 22:43:40 +0530

    Created func to generate roadmap which performs at background

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
 from flask import Flask, request, url_for, redirect, render_template, flash
 from genai import bot
 import json
+import os
+import threading
 import time
 
 app = Flask(__name__)
 app.secret_key = "secrect_key"
+
+def generate_roadmap(role_title):
+
+    filepath = "data/user.json"
+    if os.path.exists(filepath) and os.path.getsize(filepath) > 0:
+        try:
+            with open(filepath, "r") as file:
+                roadmaps = json.load(file)
+        except json.JSONDecodeError:
+            roadmaps = []
+    else:
+        roadmaps = []
+
+    roadmap = bot(role_title)
+
+    with open("data/user.json","w") as file:
+        if roadmaps:
+            roadmaps.append(roadmap)
+            json.dump(roadmaps,file,indent=4)
+        else:
+            json.dump([roadmap],file,indent=4)
+        print("done")
+        
+            
 
 @app.route("/")
 def home():
@@ -17,7 +43,7 @@ def explore():
         company_name = request.form.get('company_name').strip()
 
         try:
-            with open('roles.json', 'r') as file:
+            with open('data/roles.json', 'r') as file:
                 roles_data = json.load(file)
         except (FileNotFoundError, json.JSONDecodeError):
             roles_data = {"company": "", "response": []}
@@ -28,17 +54,17 @@ def explore():
             # will call bot here
             fetched_data = {} # genai data
             # NOTE when fetching bot data add company name to it before writing it to file
-            with open('roles.json','w') as file:
+            time.sleep(1) # time taken by bot to response
+            with open('data/roles.json','w') as file:
                 json.dump(fetched_data,file)
 
-        time.sleep(1)
 
 
         return render_template("explore.html",data = fetched_data,company = company_name)
         
     if company_name :
         try:
-            with open('roles.json', 'r') as file:
+            with open('data/roles.json', 'r') as file:
                 roles_data = json.load(file)
                 fetched_data = roles_data.get("response", [])
         except:
@@ -53,12 +79,19 @@ def roadmap():
         role = request.form.get("role_title")
         company_name = request.form.get("company_name")
 
-        flash("Target Added! Your roadmap will be available on your dashboard . ✨", "success")
-        # --- roadmap generation background bot here ---
+        flash("Target Added! Your roadmap will be available on your dashboard .", "success")
+        
+        task = threading.Thread(
+            target=generate_roadmap, 
+            args=(role,)
+            )
+        task.start()
         
         return redirect(url_for("explore",company = company_name))
         
     return redirect(url_for("explore"))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
